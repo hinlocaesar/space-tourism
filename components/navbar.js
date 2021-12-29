94c316afe6aa99d1018bb975b3997bbe4526e6ae
Caesar <caesar.hinlo.tech@gmail.com>
2021-12-29 13:41:36 +0800

    Save narbar modification with logo and hamburger

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import Image from "next/image";
+import Logo from "../assets/shared/logo.svg";
+import Hamburger from "../assets/shared/icon-hamburger.svg";
+import styles from "../styles/Navbar.module.css";
 function navbar() {
-  return <div>NAVBAR</div>;
+  return (
+    <>
+      <div className={styles.navbar}>
+        <Image src={Logo} alt="Logo" />
+        <Image src={Hamburger} alt="Hamburger" q />
+      </div>
+    </>
+  );
 }
 
 export default navbar;
