5ce8fca27b99578510b3e8ba32d4e23ab54d719a
Caesar <caesar.hinlo.tech@gmail.com>
2021-12-29 14:44:32 +0800

    save converted sized from px to em of font

--- components/home.js
+++ components/home.js
@@ -13,6 +13,18 @@ function home() {
         objectFit="cover"
         quality={100}
       />
+      <div className={styles.home__content}>
+        <div className={styles.home__title1}>So, you want to travel to</div>
+        <div className={styles.home__title2}> Space</div>
+        <div className={styles.home__description}>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </div>
+
+        <div className={styles.explorebtn}> Explore</div>
+      </div>
     </>
   );
 }
