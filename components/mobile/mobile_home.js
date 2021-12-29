fa26f456347b46d00245fa0b2d8edaeec9620a9a
Caesar <caesar.hinlo.tech@gmail.com>
2021-12-29 19:29:12 +0800

    Applied fix to home component

--- components/mobile/mobile_home.js
+++ components/mobile/mobile_home.js
@@ -1,11 +1,21 @@
 import React from "react";
-
+import Image from "next/image";
+import BackgroundImgMobileMain from "../../assets/home/background-home-mobile.jpg";
+import homestyles from "../../styles/Home.module.css";
 import styles from "../../styles/Home.module.css";
+
 function home() {
   return (
     <>
-     
       <div className={styles.home__content}>
+        <Image
+          className={styles.backgroundImage}
+          src={BackgroundImgMobileMain}
+          alt="Picture of the author"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
         <div className={styles.home__title1}>So, you want to travel to</div>
         <div className={styles.home__title2}> SPACE</div>
         <div className={styles.home__description}>
