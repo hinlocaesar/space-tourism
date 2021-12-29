41dfbebd17bfbf9e27fded5752af71f4c24ec4e8
Caesar <caesar.hinlo.tech@gmail.com>
2021-12-29 16:50:27 +0800

    Save checkpoint

--- components/layout.js
+++ components/layout.js
@@ -2,11 +2,12 @@
 
 import Navbar from "./navbar";
 import MobileHome from "./mobile/mobile_home";
+import MobileDestA from "./mobile/mobile_destination_a";
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <MobileHome />
+      <MobileDestA />
       <main>{children}</main>
     </>
   );
---
+++ components/mobile/mobile_destination_a.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Image from "next/image";
+import BackgroundImgMobile from "../../assets/destination/background-destination-mobile.jpg";
+import MoonImage from "../../assets/destination/image-moon.png";
+import styles from "../../styles/Destination.module.css";
+function mobile_destination_a() {
+  return (
+    <>
+      <Image
+        className={styles.backgroundImage}
+        src={BackgroundImgMobile}
+        alt="Picture of the author"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+      />
+      <div className={styles.content}>
+        <div className={styles.title1}>01 Pick your destintion</div>
+        <div className={styles.img}>
+          <Image
+            src={MoonImage}
+            width={170}
+            height={170}
+            alt="image of the moon"
+          />
+        </div>
+      </div>
+      <div className={styles.dest_menu}>
+        <div className={styles.dest_menu_item}>MOON</div>
+        <div className={styles.dest_menu_item}>MARS</div>
+        <div className={styles.dest_menu_item}>EUROPA</div>
+        <div className={styles.dest_menu_item}>TITAN</div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.title2}> MOON</div>
+        <div className={styles.description}>
+          See our planet as you’ve never seen it before. A perfect relaxing trip
+          away to help regain perspective and come back refreshed. While you’re
+          there, take in some history by visiting the Luna 2 and Apollo 11
+          landing sites.
+        </div>
+        <div className={styles.explorebtn}> EXPLORE</div>
+      </div>
+    </>
+  );
+}
+
+export default mobile_destination_a;
