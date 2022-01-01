bc8f023a580f7b80653960ec97ba497bd5547248
Caesar <caesar.hinlo.tech@gmail.com>
2022-01-01 20:45:42 +0800

    save modification

--- components/navbar.js
+++ components/navbar.js
@@ -12,6 +12,15 @@ function navbar() {
         <div className={styles.isMobile}>
           <Image src={Hamburger} alt="Hamburger" />
         </div>
+
+        <div className={styles.isTablet}>
+          <div className={styles.bigNav} >
+            <div className={styles.bigNavItem}>HOME</div>
+            <div className={styles.bigNavItem}>DESTINATION</div>
+            <div className={styles.bigNavItem}>CREW</div>
+            <div className={styles.bigNavItem}>TECHNOLOGY</div>
+          </div>
+        </div>
       </div>
     </>
   );
