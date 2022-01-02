6551fa1c9ae595cb01e82d3632431be55afdff7d
Caesar <caesar.hinlo.tech@gmail.com>
2022-01-02 17:28:57 +0800

    Save modification for the crew A

--- pages/crew_a.js
+++ pages/crew_a.js
@@ -23,17 +23,19 @@ export default function Home() {
         <NavBar />
         <div className={commonstyles.content}>
           <div className={commonstyles.title1}>02 Meet your crew</div>
-          <div className={styles.img}>
-            <div className={styles.imgWrapper}>
-              <Image
-                src={CrewImage}
-                alt="image of the crew commander"
-                height={224}
-                width={158}
-              />
+          <div className={styles.applyBgSwitchMobileImage}>
+            <div className={styles.img}>
+              <div className={styles.imgWrapper}>
+                <Image
+                  src={CrewImage}
+                  alt="image of the crew commander"
+                  height={224}
+                  width={158}
+                />
+              </div>
+              <div className={styles.line}> </div>
             </div>
           </div>
-          <div className={styles.line}> </div>
 
           <div className={styles.dest_menu}>
             <div className={styles.dest_menu_item}></div>
@@ -49,6 +51,27 @@ export default function Home() {
             Douglas Gerald Hurley is an American engineer, former Marine Corps
             pilot and former NASA astronaut. He launched into space for the
             third time as commander of Crew Dragon Demo-2.
+          </div>
+
+          <div className={styles.dest_menu1}>
+            <div className={styles.dest_menu_item}></div>
+            <div className={styles.dest_menu_item}></div>
+            <div className={styles.dest_menu_item}></div>
+            <div className={styles.dest_menu_item}></div>
+          </div>
+
+          <div className={styles.applyBgSwitchTabletImage}>
+            <div className={styles.img}>
+              <div className={styles.imgWrapper}>
+                <Image
+                  src={CrewImage}
+                  alt="image of the crew commander"
+                  height={572}
+                  width={456.37}
+                  quality={100}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </main>
